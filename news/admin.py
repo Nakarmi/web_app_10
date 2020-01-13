f53a0257521499978586ff58e240178135728d53
Nakarmi <bpin.nakarmi77@gmail.com>
2020-01-13 08:09:01 +0545

    Updated News

--- news/admin.py
+++ news/admin.py
@@ -4,5 +4,10 @@ from news.models import News, Category
 
 # Register your models here.
 
-admin.site.register(News)
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "created_at")
+    prepopulated_fields = {"slug": ("title",)}
+    # readonly_fields = ("author",)
+
 admin.site.register(Category)
