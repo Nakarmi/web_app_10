de9d020c3c00b7f9b9e61703340cb208c433365e
Nakarmi <bpin.nakarmi77@gmail.com>
2020-01-18 23:33:29 +0545

    news-app finished

--- news/views.py
+++ news/views.py
@@ -15,3 +15,30 @@ class CategoryNewsView(View):
     # def post(self, request, *args, **kwargs):
     #     return HttpResponse('POST request!')
 
+class NewsTemplateView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.all()
+        print(categories)
+        category_news_list = {}
+        for category in categories:
+            # context[category.title] = News.objects.filter(category=category)
+            category_news_list[category] = News.objects.filter(category=category)
+        context["news_list"] = News.objects.all().order_by("-created_at")[:4]
+        context["trending_news"] = News.objects.order_by("-count")
+        context["category_news_list"] = category_news_list
+        print(context)
+        return context
+
+class NewsDetail(DetailView):
+    model = News
+    template_name = "news/single_news.html"
+    context_object_name = "detail_news"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.object.count = self.object.count + 1
+        self.object.save()
+        context["popular_news"] = News.objects.order_by("-count")[:4]
+        return context
--- web_app_10/urls.py
+++ web_app_10/urls.py
@@ -15,13 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from news import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('', views.home, name='home'),
+#     path('news/', include('news.urls')),
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    # path('', views.home, name='home'),
+    path("", views.NewsTemplateView.as_view(), name="home"),
     path('news/', include('news.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
